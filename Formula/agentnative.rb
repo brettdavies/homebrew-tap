5d576369a679c68c07f055cb001e973a12a86f49
Brett <26392883+brettdavies@users.noreply.github.com>
2026-04-23 00:58:33 +0000

    agentnative: add 0.1.3 bottle.

--- Formula/agentnative.rb
+++ Formula/agentnative.rb
@@ -6,6 +6,13 @@ class Agentnative < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/brettdavies/agentnative-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/brettdavies/agentnative-cli/releases/download/v0.1.3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "098954be98dea0a811ebd366f12dc15ce683c74da180ed7e66fb5897748555e7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ddabdcdf5ff82e72505edf6ac674d4f8302c1375472990391cee460255dc159"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3597cc83e1917f4904554d1ec2bf68af3e42edcf2f296c0958847a2883178522"
+  end
+
   depends_on "rust" => :build
 
   def install
