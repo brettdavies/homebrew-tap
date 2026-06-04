1f6d99e0f6f86c326b0f8441c1723bef74dd42fc
Brett <26392883+brettdavies@users.noreply.github.com>
2026-06-04 19:46:37 +0000

    bird: add 0.2.0 bottle.

--- Formula/bird.rb
+++ Formula/bird.rb
@@ -6,6 +6,13 @@ class Bird < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/brettdavies/bird.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/brettdavies/bird/releases/download/v0.2.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79c6dbf8d8eb11bb6a656436b7e2f77c2bcb6b1d53fbd94cce3b13abf8bd917a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e62200af2d17f0180c66600e42ff997d4728e39ff616f03906ced4fbd2bc28f"
+    sha256 cellar: :any,                 x86_64_linux:  "54dba2fa0fbc9e67bae5faf9f663e0e3b4ad4f206cc7156a8ef3fe64adde57a7"
+  end
+
   depends_on "rust" => :build
 
   def install
