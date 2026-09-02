b8d823a7d189343496365f289bb1eb3e1956f5d0
Brett <26392883+brettdavies@users.noreply.github.com>
2026-09-02 22:34:39 +0000

    xurl-rs: add 3.0.0 bottle.

--- Formula/xurl-rs.rb
+++ Formula/xurl-rs.rb
@@ -6,6 +6,13 @@ class XurlRs < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/brettdavies/xurl-rs.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/brettdavies/xurl-rs/releases/download/v3.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "10c85b24920d5b6771c5c1fb1fa955a191212578cf6a8b5fb98c59771761b8bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7252743d93111bfeddbd57e6a7b05c2e38936bf123e6dab1ade9b67c52caca7f"
+    sha256 cellar: :any,                 x86_64_linux:  "956045f472927c808bd6986cb9df98660b85c2d259f440503455133464fb3b61"
+  end
+
   depends_on "rust" => :build
 
   def install
